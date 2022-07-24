47d831fdf6af856390bb135a8f5a35ff65f45816
D.N.Q <ikaivolkock@gmail.com>
2022-07-24 18:55:14 +0700

    feat: initialize main

---
+++ scripts/get_img_from_csv.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+import shutil
+import tqdm
+
+
+def get_img_from_csv(type="glasses"):
+    csv_path = "/home/dnq/Working/14-far/face_classification/scripts/get_rd_%s.csv" % type
+    ds_path = "/mnt/d/Working/data/14-far/face-classification/aligned/%s" % type
+    new_ds_path = "/mnt/d/Working/data/14-far/face-classification/aligned_get_rd/%s" % type
+
+    if not os.path.exists(new_ds_path):
+        os.makedirs(new_ds_path)
+
+    data = pd.read_csv(csv_path, header=None)
+    # print(data[1])
+    c = 0
+    for img_name in tqdm.tqdm(data[1]):
+        old_path = os.path.join(ds_path, img_name)
+        new_path = os.path.join(new_ds_path, img_name)
+        if not os.path.exists(new_path):
+            shutil.copy(old_path, new_path)
+            # print(f"--> copy '{old_path}' to '{new_path}'")
+            c += 1
+    print("number of image name: ", len(data))
+    print("copied: ", c)
+
+if __name__ == "__main__":
+    get_img_from_csv(type="glasses")
+    get_img_from_csv(type="mask")
+    get_img_from_csv(type="normal")
---
+++ scripts/get_random.py
@@ -0,0 +1,56 @@
+import glob
+import os
+import pandas as pd
+import tqdm
+import random
+
+
+def get_random(dir):
+    print("---> Directory: ", os.path.basename(dir))
+    list_img = glob.glob("%s/*" % dir)
+    print(len(list_img))
+
+    list_img_name = [os.path.basename(i) for i in list_img]
+    list_img_name_1 = ["_".join(i.split("_")[:2]) for i in list_img_name]
+    # print(list_img_name_1[0])
+    print("number name: ", len(list_img_name_1))
+    list_img_name_loop = list(set(list_img_name_1))
+    print("number of loop name: ", len(list_img_name_loop))
+
+    dict_loop = {}
+    for loop in tqdm.tqdm(list_img_name_loop, total=len(list_img_name_loop)):
+        list_loop = []
+        for i in list_img_name:
+            if loop in i:
+                list_loop.append(i)
+        dict_loop[loop] = list_loop
+
+    # print(len(dict_loop.keys()))
+    # print(len(list(dict_loop.values())[0]))
+
+    data = pd.DataFrame(data={
+        "img_name": dict_loop.keys(),
+        "number": [len(i) for i in dict_loop.values()]
+    })
+
+    # print(data)
+    data.to_csv("/home/dnq/Working/14-far/face_classification/scripts/stat_%s.csv" % os.path.basename(dir))
+
+    random.seed(123456)
+    list_random = []
+    for k, v in dict_loop.items():
+        x = random.randint(0, len(v) - 1)
+        # print(x)
+        list_random.append(v[x])
+
+    data = pd.DataFrame(data=list_random)
+    data.to_csv("/home/dnq/Working/14-far/face_classification/scripts/get_rd_%s.csv" % os.path.basename(dir), header=None)
+
+    print("DONE...")
+
+if __name__ == "__main__":
+    path = "/mnt/d/Working/data/14-far/face-classification/aligned"
+    list_dir = glob.glob("%s/*" % path)
+
+    for dir in list_dir:
+        get_random(dir)
